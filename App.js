2769e29217d6a4184c3cb4ad7c0bb6762f423c00
Sidodido <99152830+Sidodido@users.noreply.github.com>
2025-02-03 10:54:28 +0100

    SIDAHMED

--- App.js
+++ App.js
@@ -4,13 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DrawerNavigation from './src/components/navigation/DrawerNavigation';
 
-import {
-  Welcome,
-  SignIn,
-  OTP,
-  OCR,
-  LogIn,
-} from './src/components/screens';
+import {Welcome, SignIn, OTP, OCR, LogIn} from './src/components/screens';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -18,15 +12,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{headerShown: false}}
-        initialRouteName="Main">
-          
+        initialRouteName="Welcome">
         <Stack.Screen name="OCRPage" component={OCR} />
 
-      
         <Stack.Screen name="Main" component={DrawerNavigation} />
         <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="LogIn" component={LogIn} />
-        
       </Stack.Navigator>
     </NavigationContainer>
   );
